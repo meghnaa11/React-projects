971283bcd5078b891d9846b9aad077af3fcfeeb8
Meghna Agrawal <meghnaagrawal10@gmail.com>
2024-04-14 15:32:27 -0400

    Added projects

---
+++ react-props-cards/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function Card(props) {
+    return(
+    <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <Avatar imgURL= {props.imgURL}/>
+        </div>
+        <div className="bottom">
+          <Detail info={props.phone}/>
+          <Detail info={props.email} />
+        </div>
+      </div>
+    );
+}
+
+export default Card;
